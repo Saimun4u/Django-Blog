9bf9bc3faec64f57ad0a680500a0fa4c7e4e59cb
Saimun <saimun4u@gmail.com>
2021-06-02 15:17:41 -0500

    Added dynamic post in the templete html

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author': 'Saimun Hassan',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 02, 2021'
+    },
+
+    {
+        'author': 'John Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'February 28, 2021'
+    }
+]
+
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
